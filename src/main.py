deb5eeb51977387fe4f67157bb8edd093dc3f9fa
timkanke <timkanke@gmail.com>
2025-05-21 12:38:52 -0400

    Add click

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
+import click
+import csv
 import logging
 
 logger = logging.getLogger(__name__)
+
+@click.command()
+@click.argument('csv_file', type=click.Path(exists=True))
+def import_csv(csv_file):
+    """Imports data from a CSV file and prints it to the console."""
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            click.echo(row)
 
 def main():
     logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
     logger.info("Hello from normalize-joural-titles!")
+    import_csv()
 
 
 if __name__ == "__main__":
