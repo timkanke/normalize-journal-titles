33040e71800eaeb54ea4f1c5e69da180c55a60cd
timkanke <timkanke@gmail.com>
2025-07-11 09:49:39 -0400

    Add issn column regex replace

--- src/njt.py
+++ src/njt.py
@@ -6,7 +6,7 @@ import re
 from pathlib import Path
 
 from njt_isxn_utils import remove_non_issn, remove_non_isbn
-from njt_requests import issn_lookup, isbn_lookup
+from njt_requests import issn_lookup, isbn_lookup_lc
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +18,9 @@ def normalize(xlsx_file):
 
     # load file
     con.execute("CREATE TABLE report AS SELECT * FROM read_xlsx(?)", [xlsx_file])
+
+    # Convert issn without hyphen to have one
+    con.sql("UPDATE report SET ISSN = regexp_replace(ISSN, '(\\d{4})(\\d{4})', '\\1-\\2') WHERE LENGTH(ISSN) = 8")
 
     # load lookup_tables
     con.execute("CREATE TABLE IF NOT EXISTS book_list AS SELECT * FROM './data_lookup_tables/book_list_wikidata.csv'")
@@ -51,26 +54,24 @@ def normalize(xlsx_file):
         logger.info("Scraped journal list is empty.")
 
     # Create isbn_list from issn_column
-    # isbn_list = remove_non_isbn(issn_column)
-    # logger.debug(isbn_list)
+    isbn_list = remove_non_isbn(issn_column)
+    logger.debug(isbn_list)
 
     # Lookup requests for title by isbn
-    # isbn_and_titles = isbn_lookup(isbn_list)
-    # logger.debug(isbn_and_titles)
-    # if isbn_and_titles:
-        # con.executemany("INSERT INTO isxn_lookup VALUES (?, ?, ?)", isbn_and_titles)
-    # else:
-        # logger.info("Scraped book list is empty.")
+    isbn_and_titles = isbn_lookup_lc(isbn_list)
+    logger.debug(isbn_and_titles)
+    if isbn_and_titles:
+        con.executemany("INSERT INTO book_list (bookLabel, isbn_13) VALUES (?, ?)", isbn_and_titles)
+    else:
+        logger.info("Scraped book list is empty.")
 
     # Save file
     path = Path(xlsx_file)
     save_xlsx_file = path.parent / (path.stem + "_NORMALIZED" + path.suffix)
-    con.sql(f"CREATE TABLE final_table AS (SELECT * FROM report LEFT OUTER JOIN journal_list on report.ISSN = journal_list.issn)")
+    con.sql(f"CREATE TABLE final_table AS (SELECT * FROM report LEFT OUTER JOIN journal_list on report.ISSN = journal_list.issn LEFT OUTER JOIN book_list on report.ISSN = book_list.isbn13)")
     con.sql(f"COPY final_table TO '{save_xlsx_file}' WITH (FORMAT xlsx, HEADER true)")
     
-    # con.sql(f"CREATE TABLE final_table AS (SELECT * FROM report LEFT OUTER JOIN journal_list on report.ISSN = journal_list.issn AND report.ISSN = journal_list.issnl)")
-    # con.sql(f"COPY (SELECT * FROM report LEFT OUTER JOIN journal_list on report.ISSN = journal_list.issn LEFT OUTER JOIN book_list on report.ISSN = book_list.isbn13) TO '{save_xlsx_file}' WITH (FORMAT xlsx, HEADER true)")
-    
+
 def main():
     logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
