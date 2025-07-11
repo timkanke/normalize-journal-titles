ac47f3f9cd53efb04f12e0a87be6cb3be2d5e0f6
timkanke <timkanke@gmail.com>
2025-07-11 09:47:51 -0400

    Add LoC lookup

--- src/njt_requests.py
+++ src/njt_requests.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+import xml.etree.ElementTree as ET
 
 from requests.exceptions import RequestException, HTTPError, ConnectionError, Timeout
 
@@ -39,7 +40,7 @@ def issn_lookup(issn_list):
     return issn_and_titles
         
 # Iterate through the transaction list and search for corresponding ISSNs and titles from Google Books
-def isbn_lookup(isbn_list):
+def isbn_lookup_google(isbn_list):
     isbn_and_titles = []
     for x_isbn in isbn_list:
     
@@ -70,5 +71,38 @@ def isbn_lookup(isbn_list):
             logger.exception("Google Books exception An error occurred")
         else:
             logger.info("Google Books Request was successful.")
+
+    return isbn_and_titles
+
+
+def isbn_lookup_lc(isbn_list):
+    isbn_and_titles = []
+    for x_isbn in isbn_list:
+    
+        try:
+            url = 'http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=' + x_isbn + '"&startRecord=1&maximumRecords=5&recordSchema=mods'
+            response = requests.get(url)
+            xml_data = response.json()
+            root = ET.fromstring(xml_data)
+            
+            # Find the title element and extract the title text
+            title_element = root.find('title')
+            if title_element is not None:
+                new_title = title_element.text
+                logger.debug(f"{x_isbn} , {new_title}")
+                isbn_and_titles.append(tuple((x_isbn, new_title)))
+            if 'error' in xml_data:
+                logger.debug(f"Error")
+                    
+        except HTTPError:
+            logger.exception("LoC exception HTTP error occurred")
+        except ConnectionError:
+            logger.exception("LoC exception Connection error occurred")
+        except Timeout:
+            logger.exception("LoC exception Timeout error occurred")
+        except RequestException:
+            logger.exception("LoC exception An error occurred")
+        else:
+            logger.info("LoC Request was successful.")
 
     return isbn_and_titles
